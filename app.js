385c9ce56752d7b76bce13de7d4cd0cf066ed382
Wlakov <koval0310@gmail.com>
2022-03-11 20:33:59 +0200

    change port

--- app.js
+++ app.js
@@ -8,6 +8,6 @@ app.get('/',function (req,res){
     res.render('index.hbs')
 });
 
-app.listen(3000,function (){
+app.listen(process.env.PORT || 3000,function (){
     console.log("Server is running");
 })
