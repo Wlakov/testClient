01900d64ca0e6d629f9112a4e89d6b022baccf73
Wlakov <koval0310@gmail.com>
2022-03-11 19:13:52 +0200

    Done
    done

---
+++ app.js
@@ -0,0 +1,13 @@
+let express=require('express');
+
+const app=express();
+const path=require('path')
+app.set("view engine", "hbs");
+
+app.get('/',function (req,res){
+    res.render('index.hbs')
+});
+
+app.listen(3000,function (){
+    console.log("Server is running");
+})
